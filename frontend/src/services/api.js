945bb5505ee1592b5d048daf903d53aea9068fd1
Braeden Treutel <braeden4328@gmail.com>
2025-12-27 21:05:57 -0600

    very basic structure for backend and frontend

---
+++ frontend/src/services/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// we can read this in from env later for different environments
+const API_BASE_URL = "http://localhost:5000/api";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
