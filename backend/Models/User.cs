b8893950584193a5d553583e947c89d551f88d81
Braeden Treutel <braeden4328@gmail.com>
2025-12-29 23:59:54 -0600

    made starting structure and a little bit of user crud operations, also new health checks

---
+++ backend/Models/User.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models;
+
+// maps to the users table in the database
+[Table("Users")]
+public class User
+{
+    [Key]
+    public int UserID { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string UserGivenName { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string UserFamilyName { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string UserEmail { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string UserPassword { get; set; }
+
+    public DateTime CreatedDateTime { get; set; } = DateTime.UtcNow;
+}
